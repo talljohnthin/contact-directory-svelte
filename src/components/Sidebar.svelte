b8957c3320edf87b637d8fc7921026b2da26da73
talljohnthin <eljustin12@gmail.com>
2023-09-05 08:29:09 +0800

    import sidebar

--- src/components/Sidebar.svelte
+++ src/components/Sidebar.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { twMerge } from "tailwind-merge";
+  import { sidebar } from "../store/sidebar";
 </script>
 
 <div class={twMerge("fixed top-0 right-0 z-10 w-[500px] bg-white h-screen p-10 shadow-xl shadow-blue-100 transition-transform", $sidebar ? "translate-x-0" : "translate-x-[100%]")}>
